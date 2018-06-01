638e52bb3959e08ce342dcd4d8ec836595ad1fbc
Jaedwin Montal <Jaedwin.Montal@gmail.com>
2018-06-01 17:53:36 -0600

    Revo

--- HealthUp/includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost"; //Change localhost to server hosted
-$dbUsername = "root";
-$dbPassword = "pass";
-$dbName = "healthup";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
